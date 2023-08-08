efbc6a727693ce8fb8cd7ca0ce7e0198a28283fd
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-08-08 15:09:30 +0545

    undo changes

--- my-project/src/component/Card.jsx
+++ my-project/src/component/Card.jsx
@@ -4,7 +4,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 export default function Card({item}) {
   return (
     <div>
-          <div className='flex flex-col justify-center align-middle items-center rounded-md border w-fit overflow-hidden shadow-xl'>
+          <div className='flex flex-col justify-center align-middle items-center rounded-md border w-fit overflow-hidden  drop-shadow-lg'>
           <img src={item?.urls.full} alt="burger" height={100} width={100} className='object-cover h-48 w-96' loading='lazy'/>
           <div>{item?.name}</div>
           </div>
